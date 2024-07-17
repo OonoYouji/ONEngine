eb7120e588fe8251b3edb6641bb0f6b273456fa9
OonoYouji <k023g0007@g.neec.ac.jp>
2024-07-17 12:01:07 +0900

    windowの生成と破棄 メインループの作成

--- main.cpp
+++ main.cpp
@@ -1,9 +1,20 @@
 #include <Windows.h>
 
+#include <WinApp.h>
+
 
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	OutputDebugStringA("ONE : Hellow\n");
+
+	ONE::WinApp* winApp = ONE::WinApp::GetInstance();
+	winApp->Initialize();
+
+	while(!winApp->ProcessMessage()) {
+
+	}
+
+	winApp->Finalize();
 
 	return 0;
 }
