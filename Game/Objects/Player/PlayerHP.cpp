51e83e7183cdc283703314d22fbad4c8e973a0f7
OonoYouji <k023g0007@g.neec.ac.jp>
2024-09-13 14:44:36 +0900

    ゲージの表示

--- Game/Objects/Player/PlayerHP.cpp
+++ Game/Objects/Player/PlayerHP.cpp
@@ -1,3 +1,4 @@
+#define	NOMINMAX
 #include "PlayerHP.h"
 
 #include <SceneManager.h>
@@ -25,6 +26,22 @@ void PlayerHP::Initialize() {
 	}
 
 	pWave_ = dynamic_cast<SinWaveDrawer*>(GameObjectManager::GetGameObject("SinWaveDrawer"));
+
+	frameColor_ = Vec4(100, 100, 100, 255) / 255.0f;
+	gaugeColor_ = Vec4(0.184f, 0.851f, 0.137f, 1.0f);
+
+	frameSprite_.resize(4);
+	for(auto& sprite : frameSprite_) {
+		sprite.reset(new Sprite);
+		sprite->Initialize("gauge", "gauge.png");
+		sprite->SetPos({ 640.0f, 100.0f, 0.0f });
+		sprite->SetSize(Vec2(1280.0f, 200.0f) / 2.5f);
+		sprite->SetColor(frameColor_);
+	}
+
+	gaugeSprite_.reset(new Sprite);
+	gaugeSprite_->Initialize("white2x2", "white2x2.png");
+
 
 
 }
@@ -64,30 +81,29 @@ void PlayerHP::Update() {
 		hpSprites_[i]->SetPos(position);
 	}
 
-	/// 心臓に合わせてドクドク動かす
-	if(pWave_) {
 
-		animationTime_ += WorldTime::DeltaTime() * (pWave_->GetAmplitude() / 100.0f);
-		for(auto& sprite : hpSprites_) {
-
-			float sinValue = std::sin(8.0f * animationTime_) * 0.5f + 0.5f;
-			sprite->SetSize(
-				Vec2::kOne * 64.0f + 
-				Vec2::kOne * (32.0f * 0.6f * Ease::Out::Elastic(sinValue)) /// ここの30を変えて大きさを調整する
-			);
-		}
-	}
+	//if(hpSprites_.size() == 0) {
+	//	SceneManager::GetInstance()->SetNextScene(SCENE_ID::RESULT);
+	//}
 
 
-	if(hpSprites_.size() == 0) {
-		SceneManager::GetInstance()->SetNextScene(SCENE_ID::RESULT);
-	}
+	CalculationGage();
+
 }
 
 void PlayerHP::FrontSpriteDraw() {
-	for(auto& sprite : hpSprites_) {
+	/*for(auto& sprite : hpSprites_) {
 		sprite->Draw(2);
+	}*/
+
+
+	gaugeSprite_->Draw();
+	for(auto& sprite : frameSprite_) {
+		sprite->Draw();
 	}
+
+
+
 }
 
 void PlayerHP::Debug() {
@@ -102,18 +118,50 @@ void PlayerHP::Debug() {
 		}
 	}
 
+	if(ImGui::TreeNodeEx("frame", ImGuiTreeNodeFlags_DefaultOpen)) {
 
-	if(ImGui::TreeNodeEx("status", ImGuiTreeNodeFlags_DefaultOpen)) {
-
-		ImGui::DragFloat2("offset", &offset_.x);
-		ImGui::DragFloat("distance", &distance_);
-		ImGui::DragInt("currentHP", reinterpret_cast<int*>(&currentHP_), 0);
-		ImGui::DragInt("maxHP", reinterpret_cast<int*>(&maxHP_), 0);
+		ImGui::ColorEdit4("color", &frameColor_.x);
 
 		ImGui::TreePop();
 	}
+
+
+	if(ImGui::TreeNodeEx("gauge", ImGuiTreeNodeFlags_DefaultOpen)) {
+
+		ImGui::DragFloat("currentGauge", &currentGauge_, 0.1f);
+		ImGui::DragFloat("maxGauge", &maxGauge_, 0.1f);
+
+		ImGui::ColorEdit4("color", &gaugeColor_.x);
+
+		ImGui::TreePop();
+	}
+
 }
 
 bool PlayerHP::GetHPFluctuation() {
 	return fluctuationHP_;
+}
+
+void PlayerHP::CalculationGage() {
+
+	/// フレームの色をセット
+	for(auto& sprite : frameSprite_) {
+		sprite->SetColor(frameColor_);
+	}
+
+	float lerpT = std::min(currentGauge_ / maxGauge_, 1.0f);
+
+	gaugeSprite_->SetPos(Vec3(
+		std::lerp(640.0f - (952.0f / 2.0f), 640.0f, lerpT),
+		100.0f, 0.0f
+	));
+
+	gaugeSprite_->SetSize(Vec2(
+		std::lerp(0.0f, 952.0f / 2.0f, lerpT),
+		60.0f / 2.0f
+	));
+
+	gaugeSprite_->SetColor(gaugeColor_);
+
+
 }
--- Game/Objects/Player/PlayerHP.h
+++ Game/Objects/Player/PlayerHP.h
@@ -19,6 +19,8 @@ public:
 
 	bool GetHPFluctuation();
 
+	void CalculationGage();
+
 private:
 	std::vector<std::unique_ptr<Sprite>> hpSprites_;
 	uint32_t currentHP_;
@@ -29,5 +31,21 @@ private:
 
 	class SinWaveDrawer* pWave_ = nullptr;
 	float animationTime_ = 0.0f;
+
+
+	/// =======================================
+	/// ゲージ版の HP 表示
+	/// =======================================
+
+	std::vector<std::unique_ptr<Sprite>> frameSprite_;
+	std::unique_ptr<Sprite> gaugeSprite_;
+
+	float maxGauge_;
+	float currentGauge_;
+
+	Vec3 gaugeOffset_{};
+
+	Vec4 frameColor_{ 1,1,1,1 };
+	Vec4 gaugeColor_{ 1,1,1,1 };
 
 };
