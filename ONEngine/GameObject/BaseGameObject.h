a91af9a4e118ade85d46725f3724a0cf3cbf1a28
OonoYouji <k023g0007@g.neec.ac.jp>
2024-09-24 10:43:42 +0900

    base game object のヘッダを見やすくする

--- ONEngine/GameObject/BaseGameObject.h
+++ ONEngine/GameObject/BaseGameObject.h
@@ -21,13 +21,29 @@ public:
 	BaseGameObject();
 	virtual ~BaseGameObject() = default;
 
+	/// ===================================================
+	/// public : pure virtual methods
+	/// ===================================================
 
 	virtual void Initialize() = 0;
+	virtual void Update()     = 0;
 
-	virtual void Update() = 0;
+
+	/// ===================================================
+	/// public : virtual methods
+	/// ===================================================
+
 	virtual void LastUpdate() {}
+	virtual void Debug()      {};
 
-	virtual void Debug() {};
+	virtual void OnCollisionEnter([[maybe_unused]] BaseGameObject* const collision) {}
+	virtual void OnCollisionStay( [[maybe_unused]] BaseGameObject* const collision) {}
+	virtual void OnCollisionExit( [[maybe_unused]] BaseGameObject* const collision) {}
+
+
+	/// ===================================================
+	/// public : methods
+	/// ===================================================
 
 	/// <summary>
 	/// 行列の更新
@@ -48,55 +64,11 @@ public:
 
 	void RenameComponents();
 
-#pragma region Transform
-	void SetPositionX(float x) { pTranform_->position.x = x; }
-	void SetPositionY(float y) { pTranform_->position.y = y; }
-	void SetPositionZ(float z) { pTranform_->position.z = z; }
-	void SetPosition(const Vec3& v) { pTranform_->position = v; }
-
-	void SetRotateX(float x) { pTranform_->rotate.x = x; }
-	void SetRotateY(float y) { pTranform_->rotate.y = y; }
-	void SetRotateZ(float z) { pTranform_->rotate.z = z; }
-	void SetRotate(const Vec3& v) { pTranform_->rotate = v; }
-
-	void SetScaleX(float x) { pTranform_->scale.x = x; }
-	void SetScaleY(float y) { pTranform_->scale.y = y; }
-	void SetScaleZ(float z) { pTranform_->scale.z = z; }
-	void SetScale(const Vec3& v) { pTranform_->scale = v; }
-
-	const Vec3 GetPosition() const;
-	const Vec3 GetRotate() const { return pTranform_->rotate; }
-	const Vec3 GetScale() const { return pTranform_->scale; }
-
-	const Mat4& GetMatTransform() const { return pTranform_->matTransform; };
-	const Transform& GetTransform() const { return *pTranform_; }
-#pragma endregion
-
-#pragma region Parent Child
-	void SetParent(BaseGameObject* parent);
-	BaseGameObject* GetParent() const;
-	void AddChild(BaseGameObject* child);
-	const std::list<BaseGameObject*>& GetChilds() const;
-#pragma endregion Parent Child
-
-#pragma region Accesser Tag Name
-	void SetTag(const std::string& tag);
-	const std::string& GetTag() const;
-	void SetName(const std::string& name);
-	const std::string& GetName() const;
-#pragma endregion Accesser Tag Name
-
-#pragma region Collider 
-	virtual void OnCollisionEnter([[maybe_unused]] BaseGameObject* const collision) {}
-	virtual void OnCollisionStay( [[maybe_unused]] BaseGameObject* const collision) {}
-	virtual void OnCollisionExit( [[maybe_unused]] BaseGameObject* const collision) {}
-#pragma endregion Collider
 
 
-
-	/// ===================================================
+	/// ---------------------------------------------------
 	/// コンポーネントの追加
-	/// ===================================================
+	/// ---------------------------------------------------
 	template<typename T, typename std::enable_if<std::is_base_of<BaseComponent, T>::value>::type* = nullptr>
 	T* AddComponent() {
 		auto addComponent = std::make_unique<T>();
@@ -109,9 +81,9 @@ public:
 	}
 
 
-	/// ===================================================
+	/// ---------------------------------------------------
 	/// コンポーネントのゲッタ
-	/// ===================================================
+	/// ---------------------------------------------------
 	template<typename T, typename std::enable_if<std::is_base_of<BaseComponent, T>::value>::type* = nullptr>
 	T* GetComponent() {
 		for(auto& component : components_) {
@@ -129,24 +101,94 @@ public:
 		return components_;
 	}
 
+
+
+	/// ===================================================
+	/// public : accessor
+	/// ===================================================
+
+
+	/// ---------------------------------------------------
+	/// public : tarnsform accessor
+	/// ---------------------------------------------------
+
+	void SetPositionX(float x) { pTranform_->position.x = x; }
+	void SetPositionY(float y) { pTranform_->position.y = y; }
+	void SetPositionZ(float z) { pTranform_->position.z = z; }
+	void SetPosition(const Vec3& v) { pTranform_->position = v; }
+
+	void SetRotateX(float x)   { pTranform_->rotate.x = x; }
+	void SetRotateY(float y)   { pTranform_->rotate.y = y; }
+	void SetRotateZ(float z)   { pTranform_->rotate.z = z; }
+	void SetRotate(const Vec3& v) { pTranform_->rotate = v; }
+
+	void SetScaleX(float x)    { pTranform_->scale.x = x; }
+	void SetScaleY(float y)    { pTranform_->scale.y = y; }
+	void SetScaleZ(float z)    { pTranform_->scale.z = z; }
+	void SetScale(const Vec3& v) { pTranform_->scale = v; }
+
+	const Vec3 GetPosition()        const;
+	const Vec3 GetRotate()          const { return pTranform_->rotate; }
+	const Vec3 GetScale()           const { return pTranform_->scale; }
+
+	const Mat4& GetMatTransform()   const { return pTranform_->matTransform; };
+	const Transform& GetTransform() const { return *pTranform_; }
+
+
+
+	/// ---------------------------------------------------
+	/// public : parent accessor
+	/// ---------------------------------------------------
+
+	void SetParent(BaseGameObject* parent);
+	BaseGameObject* GetParent() const;
+
+	void AddChild(BaseGameObject* child);
+	const std::list<BaseGameObject*>& GetChilds() const;
+
+
+
+	/// ---------------------------------------------------
+	/// public : tag, name  accessor
+	/// ---------------------------------------------------
+
+	void SetTag(const std::string& tag);
+	const std::string& GetTag() const;
+
+	void SetName(const std::string& name);
+	const std::string& GetName() const;
+
 private:
-	std::string tag_;	//- グループごとの名前 : Enemyなど
-	std::string name_;	//- オブジェクトごとに違う名前 : Enemy1, Enemy2など
+	/// ===================================================
+	/// private : objects
+	/// ===================================================
+
+	std::string tag_; 
+	std::string name_;
+
+
 protected:
+	/// ===================================================
+	/// protected : objects
+	/// ===================================================
 
 	/// components内のtransformへのポインタ
-	Transform* pTranform_;
+	Transform*                 pTranform_ = nullptr;
 
-	/// 親
-	BaseGameObject* parent_ = nullptr;
+	BaseGameObject*            parent_    = nullptr;
 	std::list<BaseGameObject*> childs_;
 
 	std::list<std::unique_ptr<BaseComponent>> components_;
 
+
 public:
+	/// ===================================================
+	/// public : objects
+	/// ===================================================
 
 	bool isActive = true;
 	int drawLayerId = 0;
+
 
 };
 
