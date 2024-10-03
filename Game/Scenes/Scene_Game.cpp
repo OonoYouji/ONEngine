99149859e3aabaa20de2686de71870d03d82bd49
ku-rihara <k023g0027@g.neec.ac.jp>
2024-10-03 12:12:09 +0900

    カメラ追従途中

--- Game/Scenes/Scene_Game.cpp
+++ Game/Scenes/Scene_Game.cpp
@@ -5,8 +5,9 @@
 
 #include "GameCamera.h"
 #include "Particle/ParticleSystem.h"
-
+//class
 #include"Player/PlayerPivot.h"
+#include"GameCamera.h"
 #include "Player/Player.h"
 #include"Ground/Ground.h"
 
@@ -19,13 +20,16 @@ void Scene_Game::Initialize() {
 
 	Player* p1 = new Player;
 	PlayerPivot* playerPivot = new PlayerPivot;
+	/*GameCamera* gameCamera = new GameCamera("PlayerCamear");*/
 	Ground* ground = new Ground;
 	playerPivot->Initialize();
 	p1->Initialize();
 	ground->Initialize();
+	/*gameCamera->Initialize();*/
 	
 
 	p1->SetParent(playerPivot);
+	/*mainCamera_->SetParent(p1);*/
 
 	mainCamera_->SetPosition({ 0.0f, 0.0f, -6.49f * 4 });
 	mainCamera_->SetRotate({ 0.066f, -0.258f, 0.0f });
