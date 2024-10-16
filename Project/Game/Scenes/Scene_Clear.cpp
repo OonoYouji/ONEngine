55ce5c792430a7906233e2d6ba546b0703aca2ab
OonoYouji <k023g0007@g.neec.ac.jp>
2024-10-16 22:33:22 +0900

    game cameraを仮にアニメーションが見やすい位置に変更

--- Project/Game/Scenes/Scene_Clear.cpp
+++ Project/Game/Scenes/Scene_Clear.cpp
@@ -1,9 +1,14 @@
 #include "Scene_Clear.h"
 
+#include "Objects/Camera/GameCamera.h"
 #include "Objects/BossAnimation/BossAnimation.h"
 
 void Scene_Clear::Initialize() {
 	(new BossAnimation)->Initialize();
+
+	mainCamera_->SetPosition({ 8.0f, 3.7f, -8.8f });
+	mainCamera_->SetRotate({ 0.26f, -0.8f, 0.0f });
+	mainCamera_->GetTransform()->rotateOrder = XYZ;
 }
 
 
