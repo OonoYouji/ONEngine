6af7d45dd63d91c98694af37f98b41ded0122aca
OonoYouji <k023g0007@g.neec.ac.jp>
2024-10-28 15:22:01 +0900

    DescriptorHeapの機能実装

--- Project/ONEngine/GraphicManager/GraphicsEngine/DirectX12/DescriptorHeap/DescriptorHeap.h
+++ Project/ONEngine/GraphicManager/GraphicsEngine/DirectX12/DescriptorHeap/DescriptorHeap.h
@@ -22,7 +22,7 @@ namespace ONE {
 	/// ===================================================
 	enum class HeapType {
 		RTV,
-		UAV_CBV_SRV,
+		CBV_SRV_UAV,
 		CBV
 	};
 
@@ -36,19 +36,62 @@ namespace ONE {
 		/// public : methods
 		/// ===================================================
 
-		DescriptorHeap(uint32_t maxHeapSize) : kMaxHeapSize_(maxHeapSize) {}
+		DescriptorHeap(uint32_t _maxHeapSize) : kMaxHeapSize_(_maxHeapSize) {}
 		~DescriptorHeap() {}
 
+		/// <summary>
+		/// 初期化関数
+		/// </summary>
+		/// <param name="_dxCommon"> : dxCommonへのポインター </param>
 		void Initialize(DxCommon* _dxCommon);
+
+
+
+		uint32_t Allocate();
+
+
+		D3D12_CPU_DESCRIPTOR_HANDLE GetCPUDescriptorHandel(uint32_t _index) {
+			D3D12_CPU_DESCRIPTOR_HANDLE cpuHandle = descriptorHeap_->GetCPUDescriptorHandleForHeapStart();
+			cpuHandle.ptr += (descriptorSize_ * _index);
+			return cpuHandle;
+		}
+
+		D3D12_GPU_DESCRIPTOR_HANDLE GetGPUDescriptorHandel(uint32_t _index) {
+			D3D12_GPU_DESCRIPTOR_HANDLE gpuHandle = descriptorHeap_->GetGPUDescriptorHandleForHeapStart();
+			gpuHandle.ptr += (descriptorSize_ * _index);
+			return gpuHandle;
+		}
+
+
+		/// <summary>
+		/// コマンドリストにDescriptorHeapをセットする
+		/// </summary>
+		/// <param name="_commandList"></param>
+		void BindToCommandList(ID3D12GraphicsCommandList* _commandList) {
+			ID3D12DescriptorHeap* heaps[] = { descriptorHeap_.Get() };
+			_commandList->SetDescriptorHeaps(1, heaps);
+		}
 
 	private:
 
 		ComPtr<ID3D12DescriptorHeap> descriptorHeap_ = nullptr;
+
+		/// heapのmaxと1つあたりsize
 		const uint32_t kMaxHeapSize_;
-		uint32_t descriptorSize_;
+		uint32_t       descriptorSize_;
+
+		uint32_t useIndex_;
+
 
 		/// other class pointer
 		DxCommon* pDxCommon_ = nullptr;
 	};
+
+
+
+	/// ===================================================
+	/// DescriptorHeapの生成
+	/// ===================================================
+	ComPtr<ID3D12DescriptorHeap> CreateHeap(ID3D12Device* device, D3D12_DESCRIPTOR_HEAP_TYPE heapType, UINT numDescriptors, bool shaderVisible);
 
 }
