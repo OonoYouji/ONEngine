459fb8c1cf51282296a551a68174947bb6c302a2
OonoYouji <k023g0007@g.neec.ac.jp>
2024-10-29 10:05:04 +0900

    パーティクルの場を作る、ブランチの分け別れ

--- Project/ONEngine/ComponentManager/ParticleSystem/ParticleField.cpp
+++ Project/ONEngine/ComponentManager/ParticleSystem/ParticleField.cpp
@@ -1,5 +1,24 @@
 #include "ParticleField.h"
 
-void ParticleField::Initialize() {}
+/// engine
+#include "FrameManager/Time.h"
 
-void ParticleField::Update() {}
+#include "ParticleSystem.h"
+
+
+void ParticleField::Initialize() {
+
+	SetUpdateFunction([](Particle* particle) {
+		Transform* transform = particle->GetTransform();
+		transform->position += Vec3(0, 1, 0) * Time::DeltaTime();
+	});
+
+}
+
+void ParticleField::Update() {
+
+}
+
+void ParticleField::SetUpdateFunction(const std::function<void(Particle*)>& _updateFunction) {
+	updateFunction_ = _updateFunction;
+}
--- Project/ONEngine/ComponentManager/ParticleSystem/ParticleField.h
+++ Project/ONEngine/ComponentManager/ParticleSystem/ParticleField.h
@@ -3,6 +3,9 @@
 /// std
 #include <functional>
 
+/// lib
+#include "Math/Vector3.h"
+
 class Particle;
 
 class ParticleField {
@@ -14,8 +17,16 @@ public:
 	void Initialize();
 	void Update();
 
+	/// <summary>
+	/// fieldに入っているparticleの処理を行う関数
+	/// </summary>
+	/// <param name="_updateFunction"></param>
+	void SetUpdateFunction(const std::function<void(Particle*)>& _updateFunction);
+
 private:
 
 	std::function<void(Particle*)> updateFunction_;
+
+	Vec3 minPosition_, maxPosition_; /// fieldの最小と最大座標
 
 };
