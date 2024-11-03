099b29b8260447443227c08f8906231932e5151e
OonoYouji <k023g0007@g.neec.ac.jp>
2024-11-03 11:50:11 +0900

    jsonを持ってくる

--- Project/ONEngine/VariableManager/VariableManager.cpp
+++ Project/ONEngine/VariableManager/VariableManager.cpp
@@ -8,3 +8,7 @@ void VariableManager::Initialize() {
 void VariableManager::Finalize() {
 
 }
+
+void VariableManager::SaveJson(const std::string& filePath, const std::string& groupName) {
+
+}
--- Project/ONEngine/VariableManager/VariableManager.h
+++ Project/ONEngine/VariableManager/VariableManager.h
@@ -1,10 +1,28 @@
 #pragma once
 
+/// std
+#include <string>
+#include <variant>
+#include <unordered_map>
 
+/// lib
+#include "Math/Vector2.h"
+#include "Math/Vector3.h"
+#include "Math/Vector4.h"
+
+
+
+/// ===================================================
+/// 変数をjsonで管理するクラス
+/// ===================================================
 class VariableManager final {
 	VariableManager() {}
 	~VariableManager() {}
 public:
+
+	/// ===================================================
+	/// public : methods
+	/// ===================================================
 
 	static VariableManager* GetInstance() {
 		static VariableManager instance;
@@ -13,6 +31,23 @@ public:
 
 	void Initialize();
 	void Finalize();
+
+	void ImGui();
+
+
+	void SaveJson(const std::string& filePath, const std::string& groupName);
+
+
+private:
+
+	/// ===================================================
+	/// private : objects
+	/// ===================================================
+
+	using Item  = std::variant<int, float, bool, Vec2, Vec3, Vec4>;
+	using Group = std::unordered_map<std::string, Item>;
+
+	std::unordered_map<std::string, Group> groupArray_;
 
 private:
 	VariableManager(const VariableManager&) = delete;
