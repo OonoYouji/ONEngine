685648c497ee84b05c6097fbd8accc3aa4d15a52
OonoYouji <k023g0007@g.neec.ac.jp>
2024-11-07 17:53:55 +0900

    mesh rendererのmaterialの設定にlightingのsetterを追加

--- Project/ONEngine/ComponentManager/MeshRenderer/MeshRenderer.h
+++ Project/ONEngine/ComponentManager/MeshRenderer/MeshRenderer.h
@@ -26,21 +26,26 @@ public:
 	/// <summary>
 	/// モデルのセット
 	/// </summary>
-	void SetModel(const std::string& filePath);
-	void SetModel(Model* model);
+	void SetModel(const std::string& _filePath);
+	void SetModel(Model* _model);
 
 	/// <summary>
 	/// マテリアルのセット
 	/// </summary>
 	/// <param name="texName">: "./Resources/Textures/" からのfilePath</param>
-	void SetMaterial(const std::string& texName);
+	void SetMaterial(const std::string& _texName);
 
+	/// <summary>
+	/// ライティングをするか設定
+	/// </summary>
+	/// <param name="_isLighting"> : true -> する;  false -> しない </param>
+	void SetIsLighting(bool _isLighting);
 
 	/// <summary>
 	/// マテリアルの色のセット
 	/// </summary>
 	/// <param name="color"></param>
-	void SetColor(const Vec4& color);
+	void SetColor(const Vec4& _color);
 
 	/// <summary>
 	/// マテリアルの色のゲット
