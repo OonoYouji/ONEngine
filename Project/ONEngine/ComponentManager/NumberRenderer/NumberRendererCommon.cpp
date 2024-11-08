a2e5b7d3762de8f6578a6684ea9e1d234dfbd140
OonoYouji <k023g0007@g.neec.ac.jp>
2024-11-08 15:30:09 +0900

    仮commit

--- Project/ONEngine/ComponentManager/NumberRenderer/NumberRendererCommon.cpp
+++ Project/ONEngine/ComponentManager/NumberRenderer/NumberRendererCommon.cpp
@@ -15,13 +15,21 @@ void NumberRendererCommon::Initialize() {
 	pipelineState_->SetFillMode(kSolid);
 	pipelineState_->SetTopologyType(D3D12_PRIMITIVE_TOPOLOGY_TYPE_TRIANGLE);
 
+
 	/// input layout setting
 	pipelineState_->AddInputElement("POSITION", 0, DXGI_FORMAT_R32G32B32A32_FLOAT);
 	pipelineState_->AddInputElement("TEXCOORD", 0, DXGI_FORMAT_R32G32_FLOAT);
 
+
 	/// buffer setting
 	pipelineState_->AddCBV(D3D12_SHADER_VISIBILITY_VERTEX, 0); /// view projection
-	pipelineState_->AddCBV(D3D12_SHADER_VISIBILITY_VERTEX, 1); /// transform
+
+	pipelineState_->AddDescriptorRange(0, 1, D3D12_DESCRIPTOR_RANGE_TYPE_SRV);
+	pipelineState_->AddDescriptorRange(0, 1, D3D12_DESCRIPTOR_RANGE_TYPE_SRV);
+
+	pipelineState_->AddDescriptorTable(D3D12_SHADER_VISIBILITY_VERTEX, 0); /// transform 
+	pipelineState_->AddDescriptorTable(D3D12_SHADER_VISIBILITY_PIXEL, 0);  /// texture
+	pipelineState_->AddStaticSampler(D3D12_SHADER_VISIBILITY_PIXEL, 0);
 
 	pipelineState_->Initialize();
 
@@ -29,4 +37,16 @@ void NumberRendererCommon::Initialize() {
 
 void NumberRendererCommon::Finalize() {
 	pipelineState_.reset();
+}
+
+
+
+void NumberRendererCommon::PreDraw() {
+
+}
+
+void NumberRendererCommon::PostDraw() {
+
+	pipelineState_->SetPipelineState();
+
 }
