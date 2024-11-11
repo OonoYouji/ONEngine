d8b4f1a5b12afe108a17bd8a1dc9e683fb30b7ba
OonoYouji <k023g0007@g.neec.ac.jp>
2024-11-11 17:06:47 +0900

    ゲームのフレームワークを作成

--- Project/main.cpp
+++ Project/main.cpp
@@ -1,36 +1,12 @@
-/// engine
 #include "Core/ONEngine.h"
-#include "LoggingManager/Logger.h"
-/// game
-#include "Scenes/Manager/SceneManager.h"
+#include "Core/GameFrameWork.h"
 
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
-	/// 起動速度を計算する
-	std::chrono::steady_clock::time_point start, end;
-	start = std::chrono::high_resolution_clock::now();;
-	ONE::Logger::ConsolePrint("execution!!!");
-
-	/// engine, game initialize
-	SceneManager* sceneManager = SceneManager::GetInstance();
 	ONEngine::Initialize(L"DirectXGame", false, false, 60u);
-	sceneManager->Initialize(SCENE_ID::GAME); /// 初期化時のシーンをここで決定
 
-	/// 実行までにかかった時間
-	end = std::chrono::high_resolution_clock::now();
-	ONE::Logger::ConsolePrint(std::format("ExecutionTime: {}s", (end - start).count() / 1000.0f));
-
-	/// main loop
-	while(ONEngine::GetIsRunning()) {
-		/// ↓ 更新処理に移る
-		ONEngine::Update();
-		sceneManager->Update();
-
-		/// ↓ 描画処理に移る
-		ONEngine::PreDraw();
-		sceneManager->Draw();
-		ONEngine::PostDraw();
-	}
+	std::unique_ptr<GameFrameWork> gameFrameWork(new GameFrameWork);
+	gameFrameWork->Run();
 	
 	ONEngine::Finalize();
 	return 0;
