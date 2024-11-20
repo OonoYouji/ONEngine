bc8beca5141573ecb15724a662076a29bc2f3cb7
testName <k023g0068@g.neec.ac.jp>
2024-11-21 03:31:36 +0900

    歩きアニメーションを修正

--- Project/Game/Scenes/Scene_Game.cpp
+++ Project/Game/Scenes/Scene_Game.cpp
@@ -31,7 +31,7 @@ void Scene_Game::Initialize() {
 
 
 	/// モデル確認用のオブジェクト
-	(new ModelPreviewObject("Player_Wait"))->Initialize();
+	(new ModelPreviewObject("Player_Walk"))->Initialize();
 	(new ModelPreviewObject("Player_Wait"))->Initialize();
 
 
