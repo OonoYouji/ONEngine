e83867b35427233f02c74f3642f440d8f79ace3b
OonoYouji <k023g0007@g.neec.ac.jp>
2024-11-26 16:47:06 +0900

    button b uiをio

--- Project/Game/Objects/UIManager/ButtonBUI/ButtonBUI.cpp
+++ Project/Game/Objects/UIManager/ButtonBUI/ButtonBUI.cpp
@@ -25,11 +25,18 @@ void ButtonBUI::Update() {
 }
 
 void ButtonBUI::AddVariables() {
-	VariableManager::GetInstance();
+	VariableManager* vm = VariableManager::GetInstance();
+	const std::string& groupName = GetTag();
 
+	vm->AddValue(groupName, "position", pTransform_->position);
+	vm->AddValue(groupName, "scale",    pTransform_->scale);
 }
 
 void ButtonBUI::ApplyVariables() {
+	VariableManager* vm = VariableManager::GetInstance();
+	const std::string& groupName = GetTag();
 
+	pTransform_->position = vm->GetValue<Vec3>(groupName, "position");
+	pTransform_->scale    = vm->GetValue<Vec3>(groupName, "scale");
 }
 
