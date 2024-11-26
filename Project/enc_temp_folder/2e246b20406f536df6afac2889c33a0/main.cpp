3f4fb897a1dd022a93b48bcc6fdc6476f6ff2e5e
OonoYouji <k023g0007@g.neec.ac.jp>
2024-11-26 14:32:01 +0900

    ウィンドウタイトルバーを変更

--- Project/enc_temp_folder/2e246b20406f536df6afac2889c33a0/main.cpp
+++ Project/enc_temp_folder/2e246b20406f536df6afac2889c33a0/main.cpp
@@ -3,7 +3,7 @@
 
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
-	ONEngine::Initialize(L"2204_グスタフの巨人", false, false, 60u);
+	ONEngine::Initialize(L"2104_グスタフの巨人", false, false, 60u);
 
 	std::unique_ptr<GameFrameWork> gameFrameWork(new GameFrameWork);
 	gameFrameWork->Run();
