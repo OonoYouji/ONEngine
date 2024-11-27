83908f395fbaf29898ba04f5527d2831ca9a3b02
OonoYouji <k023g0007@g.neec.ac.jp>
2024-11-28 05:29:32 +0900

    ゲームの開始演出の調整項目を吐き出す

--- Project/Game/Objects/GameStartEffect/GameStartEffect.cpp
+++ Project/Game/Objects/GameStartEffect/GameStartEffect.cpp
@@ -5,6 +5,9 @@
 #include "VariableManager/VariableManager.h"
 #include "FrameManager/Time.h"
 
+/// game
+#include "ComponentManager/AudioSource/AudioSource.h"
+
 
 GameStartEffect::GameStartEffect(std::vector<BaseGameObject*>& _gameObjectVector)
 	: gameObjectVector_(_gameObjectVector) {
@@ -15,23 +18,28 @@ GameStartEffect::~GameStartEffect() {}
 
 void GameStartEffect::Initialize() {
 
+	se_ = AddComponent<AudioSource>();
+
 	/// 演出を見せるために一旦すべてのオブジェクトのアクティブを切る
 	for(auto& obj : gameObjectVector_) {
 		obj->isActive = false;
 	}
 
 	maxEffectTime_ = 7.5f;
-	/*AddVariables();
+	seTriggerTime_ = 1.0f;
+
+	AddVariables();
 	VariableManager::GetInstance()->LoadSpecificGroupsToJson(
 		"./Resources/Parameters/Objects", GetTag()
 	);
-	ApplyVariables();*/
+	ApplyVariables();
 
 }
 
 void GameStartEffect::Update() {
 	
 	isFinish_.Update();
+	isPlaySE_.Update();
 
 	//ApplyVariables();
 
@@ -42,6 +50,13 @@ void GameStartEffect::Update() {
 
 	if(lerpT == 1.0f) {
 		isFinish_.current = true;
+	}
+
+	if(!isPlaySE_.Press()) {
+		if(seTriggerTime_ <= currentEffectTime_) {
+			isPlaySE_.current = true;
+			se_->PlayOneShot("EnemyStart.wav", 0.1f);
+		}
 	}
 
 
@@ -57,6 +72,7 @@ void GameStartEffect::AddVariables() {
 	const std::string& groupName = GetTag();
 
 	vm->AddValue(groupName, "maxEffectTime", maxEffectTime_);
+	vm->AddValue(groupName, "seTriggerTime", seTriggerTime_);
 }
 
 void GameStartEffect::ApplyVariables() {
@@ -64,6 +80,7 @@ void GameStartEffect::ApplyVariables() {
 	const std::string& groupName = GetTag();
 
 	maxEffectTime_ = vm->GetValue<float>(groupName, "maxEffectTime");
+	seTriggerTime_ = vm->GetValue<float>(groupName, "seTriggerTime");
 }
 
 void GameStartEffect::StartGame() {
