b2d4589522e2a6b3bc44b7b7071c1e9eb783082c
OonoYouji <k023g0007@g.neec.ac.jp>
2024-12-01 21:04:13 +0900

    compute shader用psoクラスを追加

---
+++ Project/ONEngine/GraphicManager/PipelineState/ComputePipelineState.cpp
@@ -0,0 +1,13 @@
+#include "ComputePipelineState.h"
+
+ComputePipelineState::ComputePipelineState() {}
+
+ComputePipelineState::~ComputePipelineState() {}
+
+
+
+void ComputePipelineState::Create() {
+	CreateRootSignature();
+}
+
+void ComputePipelineState::CreateRootSignature() {}
