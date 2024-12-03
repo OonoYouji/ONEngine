3607675d0af8f06f4785a55e16c92aa506520e0a
OonoYouji <k023g0007@g.neec.ac.jp>
2024-12-03 14:02:18 +0900

    ダサいパーティクルが出た

--- Project/Game/Objects/Effect/PlayerNormalEffect/PlayerNormalEffect.cpp
+++ Project/Game/Objects/Effect/PlayerNormalEffect/PlayerNormalEffect.cpp
@@ -1,6 +1,7 @@
 #include "PlayerNormalEffect.h"
 
 /// engine
+#include "FrameManager/Time.h"
 #include "ComponentManager/ParticleSystem/ParticleSystem.h"
 
 PlayerNormalEffect::PlayerNormalEffect() {
@@ -11,13 +12,20 @@ PlayerNormalEffect::~PlayerNormalEffect() {}
 
 void PlayerNormalEffect::Initialize() {
 
-	particleSystem_ = AddComponent<ParticleSystem>(128, "PlayerNormalParticle");
+	particleSystem_ = AddComponent<ParticleSystem>(1024, "PlayerNormalParticle");
 	
 	particleSystem_->SetParticleRespawnTime(0.1f);
 	particleSystem_->SetEmittedParticleCount(7);
 	particleSystem_->SetBoxEmitterMinMax(-Vec3::kOne * 1.5f, Vec3::kOne * 1.5f);
+	particleSystem_->SetUseBillboard(false);
 
-	pTransform_->position.y = 2.0f;
+	particleSystem_->SetPartilceUpdateFunction([](Particle* _particle) {
+		Transform* transform = _particle->GetTransform();
+
+		transform->position.y += 0.25f * Time::DeltaTime();
+		transform->rotate.y   += 0.01f;
+		transform->scale       = Vec3::kOne * std::lerp(1.5f, 0.2f, 1.0f - _particle->GetNormLifeTime());
+	});
 
 }
 
