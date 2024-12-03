c5e0c33bf5bc0a43a50ce5fe276a69c570b194d7
OonoYouji <k023g0007@g.neec.ac.jp>
2024-12-03 12:09:56 +0900

    当たり判定を追加

--- Project/Game/Objects/BackgroundObjectManager/Wall/Wall.cpp
+++ Project/Game/Objects/BackgroundObjectManager/Wall/Wall.cpp
@@ -1,7 +1,9 @@
 #include "Wall.h"
 
-/// component
+/// engine
+#include "GraphicManager/ModelManager/ModelManager.h"
 #include "ComponentManager/MeshRenderer/MeshRenderer.h"
+#include "ComponentManager/Collider/BoxCollider.h"
 
 Wall::Wall() {
 	CreateTag(this);
@@ -10,8 +12,12 @@ Wall::Wall() {
 Wall::~Wall() {}
 
 void Wall::Initialize() {
+
 	meshRenderer_ = AddComponent<MeshRenderer>();
 	meshRenderer_->SetModel(GetTag());
+
+	Model*       collierModel = ModelManager::Load(GetTag());
+	BoxCollider* collider     = AddComponent<BoxCollider>(collierModel);
 }
 
 void Wall::Update() {
