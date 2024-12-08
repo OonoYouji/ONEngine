f17ef3c0f6939677ccd477865341ed7d7c112e13
OonoYouji <k023g0007@g.neec.ac.jp>
2024-12-08 23:22:50 +0900

    fix - title bar

--- Project/main.cpp
+++ Project/main.cpp
@@ -4,7 +4,7 @@
 
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
-	ONEngine::Initialize(L"DirectXGame", false, false, 60u);
+	ONEngine::Initialize(L"LE2A_04_オオノ_ヨウジ", false, false, 60u);
 
 	std::unique_ptr<GameFrameWork> gameFrameWork(new GameFrameWork);
 	gameFrameWork->Run();
