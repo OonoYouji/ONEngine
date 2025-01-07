4c47f453e676d7752589c6c2a6ce30a8b0e03c28
OonoYouji <k023g0007@g.neec.ac.jp>
2025-01-07 22:34:33 +0900

    エフェクト

--- Project/Game/Objects/HitEffect/HitEffect.cpp
+++ Project/Game/Objects/HitEffect/HitEffect.cpp
@@ -20,27 +20,28 @@ void HitEffect::Initialize() {
 	meshRenderer_ = AddComponent<MeshRenderer>();
 	meshRenderer_->SetModel("HitEffect");
 	meshRenderer_->SetMaterial("HitEffect-sheet.png");
-	meshRenderer_->SetColor({ 1,1,1,1 });
+	meshRenderer_->SetColor({ 1,1,1,1.0f });
 	meshRenderer_->SetIsLighting(false);
-	meshRenderer_->SetUVScale({ 1.0f / 4.0f, 1.0f});
+	meshRenderer_->SetUVScale({ 1.0f / 6.0f, 1.0f});
 
 	time_          = 0.0f;
-	frameDuration_ = 0.1f;
+	frameDuration_ = 0.01f;
 
 }
 
 void HitEffect::Update() {
 
 	pTransform_->rotate = pCamera_->GetRotate();
+	pTransform_->scale += Vec3::kOne * 0.5f;
 
 	time_ += Time::DeltaTime();
 	if(time_ > frameDuration_) {
 		time_ = 0.0f;
 		frameIndex_++;
-		meshRenderer_->SetUVPosition({ 1.0f / 4.0f * frameIndex_, 0.0f });
+		meshRenderer_->SetUVPosition({ 1.0f / 6.0f * frameIndex_, 0.0f });
 	}
 
-	if(frameIndex_ >= 4) {
+	if(frameIndex_ >= 6) {
 		Destory();
 		//frameIndex_ = 0;
 	}
