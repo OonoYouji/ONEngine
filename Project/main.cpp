b878e455e25170a9fb8be1242a016b25d245492d
OonoYouji <k023g0007@g.neec.ac.jp>
2025-01-16 05:47:56 +0900

    main.cppでテスト用にコンパイルしているシェーダーを削除

--- Project/main.cpp
+++ Project/main.cpp
@@ -9,18 +9,6 @@
 
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
-	{	/// shader compile test
-
-		Shader shader;
-		ShaderCompiler compiler;
-
-		compiler.Initialize();
-		shader.Initialize(&compiler);	
-
-		shader.CompileShader(L"./Resources/Shader/Vertex.vs.hlsl", L"vs_6_0", Shader::Type::vs);
-
-	}
-
 	std::unique_ptr<GameFramework> gameFramework = std::make_unique<GameFramework>();
 	gameFramework->Initialize(GameFrameworkConfig{
 		.windowName	= L"TwoEngine",
