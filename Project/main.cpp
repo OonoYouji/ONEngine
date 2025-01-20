d7fcf53d632d932fcc286bc7db8082c2ea0fb3b3
OonoYouji <k023g0007@g.neec.ac.jp>
2025-01-20 11:35:20 +0900

    mainのincludeを減らす

--- Project/main.cpp
+++ Project/main.cpp
@@ -4,9 +4,6 @@
 /// game loop
 #include "GameLoop/GameLoop.h"
 
-#include "Engine/Graphics/Shader/Shader.h"
-#include "Engine/Graphics/Shader/ShaderCompiler.h"
-
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	std::unique_ptr<GameFramework> gameFramework = std::make_unique<GameFramework>();
