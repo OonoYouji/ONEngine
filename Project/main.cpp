138a14163667987c7e44bcd226dace317c768e1d
OonoYouji <k023g0007@g.neec.ac.jp>
2025-01-21 13:44:13 +0900

    main.cppのインデント修正

--- Project/main.cpp
+++ Project/main.cpp
@@ -5,8 +5,8 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	std::unique_ptr<GameFramework> gameFramework = std::make_unique<GameFramework>();
 	gameFramework->Initialize(GameFrameworkConfig{
-		.windowName   = L"TwoEngine",
-		.windowSize   = Vec2(1280, 720),
+		.windowName = L"TwoEngine",
+		.windowSize = Vec2(1280, 720),
 	});
 
 
