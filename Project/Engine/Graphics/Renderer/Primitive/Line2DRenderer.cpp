ea4dadc93853dc190bed4c1b7aba4a87ee0537d4
OonoYouji <k023g0007@g.neec.ac.jp>
2025-01-21 12:33:36 +0900

    コメントアウトを書いた

--- Project/Engine/Graphics/Renderer/Primitive/Line2DRenderer.cpp
+++ Project/Engine/Graphics/Renderer/Primitive/Line2DRenderer.cpp
@@ -10,39 +10,44 @@ Line2DRenderer::~Line2DRenderer() {}
 
 void Line2DRenderer::Initialize(ShaderCompiler* _shaderCompiler, DxDevice* _dxDevice) {
 
-	/// shaderをコンパイル
-	Shader shader;
-	shader.Initialize(_shaderCompiler);
-	shader.CompileShader(L"Assets/Shader/Line2D.vs.hlsl", L"vs_6_0", Shader::Type::vs);
-	shader.CompileShader(L"Assets/Shader/Line2D.ps.hlsl", L"ps_6_0", Shader::Type::ps);
+	{	/// pipelineの作成
+		
+		/// shaderをコンパイル
+		Shader shader;
+		shader.Initialize(_shaderCompiler);
+		shader.CompileShader(L"Assets/Shader/Line2D.vs.hlsl", L"vs_6_0", Shader::Type::vs);
+		shader.CompileShader(L"Assets/Shader/Line2D.ps.hlsl", L"ps_6_0", Shader::Type::ps);
+
+		pipeline_.reset(new GraphicsPipeline());
+		pipeline_->SetShader(&shader);
+
+		/// input element setting
+		pipeline_->AddInputElement("POSITION", 0, DXGI_FORMAT_R32G32B32A32_FLOAT);
+		pipeline_->AddInputElement("COLOR", 0, DXGI_FORMAT_R32G32B32A32_FLOAT);
+
+		/// rasterizer desc setting
+		pipeline_->SetFillMode(D3D12_FILL_MODE_SOLID);
+		pipeline_->SetCullMode(D3D12_CULL_MODE_NONE);
+
+		/// topology type setting
+		pipeline_->SetTopologyType(D3D12_PRIMITIVE_TOPOLOGY_TYPE_LINE);
+
+		/// blend desc setting
+		D3D12_BLEND_DESC blendDesc = {};
+		blendDesc.RenderTarget[0].BlendEnable = TRUE;
+		blendDesc.RenderTarget[0].SrcBlend = D3D12_BLEND_SRC_ALPHA;
+		blendDesc.RenderTarget[0].DestBlend = D3D12_BLEND_INV_SRC_ALPHA;
+		blendDesc.RenderTarget[0].BlendOp = D3D12_BLEND_OP_ADD;
+		blendDesc.RenderTarget[0].SrcBlendAlpha = D3D12_BLEND_ONE;
+		blendDesc.RenderTarget[0].DestBlendAlpha = D3D12_BLEND_ZERO;
+		blendDesc.RenderTarget[0].BlendOpAlpha = D3D12_BLEND_OP_ADD;
+		blendDesc.RenderTarget[0].RenderTargetWriteMask = D3D12_COLOR_WRITE_ENABLE_ALL;
+		pipeline_->SetBlendDesc(blendDesc);
 
 
-	/// pipelineの作成
-	pipeline_.reset(new GraphicsPipeline());
-
-	pipeline_->SetShader(&shader);
-
-	pipeline_->AddInputElement("POSITION", 0, DXGI_FORMAT_R32G32B32A32_FLOAT);
-	pipeline_->AddInputElement("COLOR",    0, DXGI_FORMAT_R32G32B32A32_FLOAT);
-
-	pipeline_->SetFillMode(D3D12_FILL_MODE_SOLID);
-	pipeline_->SetCullMode(D3D12_CULL_MODE_NONE);
-	pipeline_->SetTopologyType(D3D12_PRIMITIVE_TOPOLOGY_TYPE_LINE);
-
-
-	D3D12_BLEND_DESC blendDesc = {};
-	blendDesc.RenderTarget[0].BlendEnable           = TRUE;
-	blendDesc.RenderTarget[0].SrcBlend              = D3D12_BLEND_SRC_ALPHA;
-	blendDesc.RenderTarget[0].DestBlend             = D3D12_BLEND_INV_SRC_ALPHA;
-	blendDesc.RenderTarget[0].BlendOp               = D3D12_BLEND_OP_ADD;
-	blendDesc.RenderTarget[0].SrcBlendAlpha         = D3D12_BLEND_ONE;
-	blendDesc.RenderTarget[0].DestBlendAlpha        = D3D12_BLEND_ZERO;
-	blendDesc.RenderTarget[0].BlendOpAlpha          = D3D12_BLEND_OP_ADD;
-	blendDesc.RenderTarget[0].RenderTargetWriteMask = D3D12_COLOR_WRITE_ENABLE_ALL;
-	pipeline_->SetBlendDesc(blendDesc);
-
-
-	pipeline_->CreatePipeline(_dxDevice);
+		/// create pipeline
+		pipeline_->CreatePipeline(_dxDevice);
+	}
 
 
 	/// verticesを最大数分メモリを確保
