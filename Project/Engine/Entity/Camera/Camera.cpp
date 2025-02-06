562fd94b39ceeb1ca5bcf36ae0e0b0468788c9e4
OonoYouji <k023g0007@g.neec.ac.jp>
2025-02-06 09:53:34 +0900

    カメラのクラスを作ってview projectionを持たせた

---
+++ Project/Engine/Entity/Camera/Camera.cpp
@@ -0,0 +1,17 @@
+#include "Camera.h"
+
+Camera::Camera() {}
+Camera::~Camera() {}
+
+
+void Camera::Initialize() {
+
+}
+
+void Camera::Update() {
+
+}
+
+void Camera::Draw() {
+
+}
---
+++ Project/Engine/Entity/Camera/Camera.h
@@ -0,0 +1,53 @@
+#pragma once
+
+/// std
+#include <memory>
+
+/// engine
+#include "../Interface/IEntity.h"
+
+#include "Engine/Graphics/Buffer/ConstantBuffer.h"
+#include "Engine/Graphics/Buffer/Data/ViewProjection.h"
+
+
+/// ===================================================
+/// カメラ
+/// ===================================================
+class Camera : public IEntity {
+public:
+
+	/// ===================================================
+	/// public : methods
+	/// ===================================================
+
+	Camera();
+	~Camera();
+
+	void Initialize() override;
+	void Update()     override;
+	void Draw()       override;
+
+private:
+	
+	/// ===================================================
+	/// private : objects
+	/// ===================================================
+
+	std::unique_ptr<ConstantBuffer<ViewProjection>> viewProjection_;
+
+
+
+public:
+
+	/// ===================================================
+	/// public : accessors
+	/// ===================================================
+
+	/// @brief view projection の取得
+	/// @return view projection data
+	const ViewProjection& GetViewProjection() const {
+		return viewProjection_->GetMappingData(); 
+	}
+
+};
+
