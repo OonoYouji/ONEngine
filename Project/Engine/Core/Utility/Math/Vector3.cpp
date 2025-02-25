106d1e76c08ea3eb537203a8606c55f72d2aa8f8
OonoYouji <k023g0007@g.neec.ac.jp>
2025-02-25 19:32:26 +0900

    関数の仮引数に_を付けた

--- Project/Engine/Core/Utility/Math/Vector3.cpp
+++ Project/Engine/Core/Utility/Math/Vector3.cpp
@@ -31,16 +31,16 @@ Vector3::Vector3() {
 	this->z = 0.0f;
 }
 
-Vector3::Vector3(float x, float y, float z) {
-	this->x = x;
-	this->y = y;
-	this->z = z;
+Vector3::Vector3(float _x, float _y, float _z) {
+	this->x = _x;
+	this->y = _y;
+	this->z = _z;
 }
 
-Vector3::Vector3(const Vector2& v, float z) {
-	this->x = v.x;
-	this->y = v.y;
-	this->z = z;
+Vector3::Vector3(const Vector2& _v, float _z) {
+	this->x = _v.x;
+	this->y = _v.y;
+	this->z = _z;
 }
 
 
@@ -57,33 +57,33 @@ Vector3 Vector3::Normalize() const {
 	return *this;
 }
 
-float Vector3::Length(const Vector3& v) {
-	return v.Len();
+float Vector3::Length(const Vector3& _v) {
+	return _v.Len();
 }
 
-Vector3 Vector3::Normalize(const Vector3& v) {
-	return v.Normalize();
+Vector3 Vector3::Normalize(const Vector3& _v) {
+	return _v.Normalize();
 }
 
-Vector3 Vector3::Lerp(const Vector3& start, const Vector3& end, float t) {
+Vector3 Vector3::Lerp(const Vector3& _start, const Vector3& _end, float _t) {
 	return Vector3(
-		std::lerp(start.x, end.x, t),
-		std::lerp(start.y, end.y, t),
-		std::lerp(start.z, end.z, t)
+		std::lerp(_start.x, _end.x, _t),
+		std::lerp(_start.y, _end.y, _t),
+		std::lerp(_start.z, _end.z, _t)
 	);
 }
 
-Vector3 Vector3::Slerp(const Vector3& start, const Vector3& end, float t) {
-	Vec3 nv1 = Normalize(start);
-	Vec3 nv2 = Normalize(end);
+Vector3 Vector3::Slerp(const Vector3& _start, const Vector3& _end, float _t) {
+	Vec3 nv1 = Normalize(_start);
+	Vec3 nv2 = Normalize(_end);
 
 	float dot = Dot(nv1, nv2);
 
 	float theta = std::acos(dot);
 
 	float sinTheta = std::sin(theta);
-	float sinThetaFrom = std::sin((1.0f - t) * theta);
-	float sinThetaTo = std::sin(t * theta);
+	float sinThetaFrom = std::sin((1.0f - _t) * theta);
+	float sinThetaTo = std::sin(_t * theta);
 
 	Vec3 nLerpVector = nv1 * (sinThetaFrom / sinTheta) + nv2 * (sinThetaTo / sinTheta);
 	if(sinTheta < 1.0e-5) {
@@ -92,30 +92,30 @@ Vector3 Vector3::Slerp(const Vector3& start, const Vector3& end, float t) {
 		nLerpVector = (nv1 * sinThetaFrom + nv2 * sinThetaTo) / sinTheta;
 	}
 
-	float length1 = Length(start);
-	float length2 = Length(end);
-	float length = std::lerp(length1, length2, t);
+	float length1 = Length(_start);
+	float length2 = Length(_end);
+	float length = std::lerp(length1, length2, _t);
 
 	return nLerpVector * length;
 }
 
-Vector3 Vector3::Cross(const Vector3& v1, const Vector3& v2) {
+Vector3 Vector3::Cross(const Vector3& _v1, const Vector3& _v2) {
 	return Vector3(
-		v1.y * v2.z - v1.z * v2.y,
-		v1.z * v2.x - v1.x * v2.z,
-		v1.x * v2.y - v1.y * v2.x
+		_v1.y * _v2.z - _v1.z * _v2.y,
+		_v1.z * _v2.x - _v1.x * _v2.z,
+		_v1.x * _v2.y - _v1.y * _v2.x
 	);
 }
 
-float Vector3::Dot(const Vector3& v1, const Vector3& v2) {
-	return (v1.x * v2.x) + (v1.y * v2.y) + (v1.z * v2.z);
+float Vector3::Dot(const Vector3& _v1, const Vector3& _v2) {
+	return (_v1.x * _v2.x) + (_v1.y * _v2.y) + (_v1.z * _v2.z);
 }
 
-Vector3 Vector3::MaxDotVector(const Vector3& direction, const std::vector<Vector3>& vertices) {
-	float maxDot = Dot(vertices.front(), direction);
-	Vec3 maxVertex = vertices.front();
-	for(auto& vertex : vertices) {
-		float dot = Dot(vertex, direction);
+Vector3 Vector3::MaxDotVector(const Vector3& _direction, const std::vector<Vector3>& _vertices) {
+	float maxDot = Dot(_vertices.front(), _direction);
+	Vec3 maxVertex = _vertices.front();
+	for(auto& vertex : _vertices) {
+		float dot = Dot(vertex, _direction);
 		if(dot > maxDot) {
 			maxDot = dot;
 			maxVertex = vertex;
@@ -125,11 +125,11 @@ Vector3 Vector3::MaxDotVector(const Vector3& direction, const std::vector<Vector
 	return maxVertex;
 }
 
-Vector3 Vector3::MinDotVector(const Vector3& direction, const std::vector<Vector3>& vertices) {
-	float minDot = Dot(vertices.front(), direction);
-	Vec3 minVertex = vertices.front();
-	for(auto& vertex : vertices) {
-		float dot = Dot(vertex, direction);
+Vector3 Vector3::MinDotVector(const Vector3& _direction, const std::vector<Vector3>& _vertices) {
+	float minDot = Dot(_vertices.front(), _direction);
+	Vec3 minVertex = _vertices.front();
+	for(auto& vertex : _vertices) {
+		float dot = Dot(vertex, _direction);
 		if(dot < minDot) {
 			minDot = dot;
 			minVertex = vertex;
@@ -138,5 +138,3 @@ Vector3 Vector3::MinDotVector(const Vector3& direction, const std::vector<Vector
 
 	return minVertex;
 }
-
-
--- Project/Engine/Core/Utility/Math/Vector3.h
+++ Project/Engine/Core/Utility/Math/Vector3.h
@@ -3,7 +3,7 @@
 
 #include <vector>
 
-class Vector2;
+#include "Vector2.h"
 
 /// ===================================================
 /// 3次元ベクトル
@@ -12,8 +12,8 @@ class Vector3 final {
 public:
 
 	Vector3();
-	Vector3(float x, float y, float z);
-	Vector3(const Vector2& v, float z);
+	Vector3(float _x, float _y, float _z);
+	Vector3(const Vector2& _v, float _z);
 	Vector3(const Vector3&) = default;
 	Vector3(Vector3&&) = default;
 
@@ -52,17 +52,17 @@ public:
 	/// public : static methods
 	/// ===================================================
 
-	static float Length(const Vector3& v);
-	static Vector3 Normalize(const Vector3& v);
+	static float Length(const Vector3& _v);
+	static Vector3 Normalize(const Vector3& _v);
 
-	static Vector3 Lerp(const Vector3& start, const Vector3& end, float t);
-	static Vector3 Slerp(const Vector3& start, const Vector3& end, float t);
+	static Vector3 Lerp(const Vector3& _start, const Vector3& _end, float _t);
+	static Vector3 Slerp(const Vector3& _start, const Vector3& _end, float _t);
 
-	static Vector3 Cross(const Vector3& v1, const Vector3& v2);
-	static float Dot(const Vector3& v1, const Vector3& v2);
+	static Vector3 Cross(const Vector3& _v1, const Vector3& _v2);
+	static float Dot(const Vector3& _v1, const Vector3& _v2);
 
-	static Vector3 MaxDotVector(const Vector3& direction, const std::vector<Vector3>& vertices);
-	static Vector3 MinDotVector(const Vector3& direction, const std::vector<Vector3>& vertices);
+	static Vector3 MaxDotVector(const Vector3& _direction, const std::vector<Vector3>& _vertices);
+	static Vector3 MinDotVector(const Vector3& _direction, const std::vector<Vector3>& _vertices);
 
 public:
 
@@ -70,92 +70,90 @@ public:
 	/// public : operator overload
 	/// ===================================================
 
-	inline Vector3& operator= (const Vector3& other);
-	inline Vector3& operator+= (const Vector3& other);
-	inline Vector3& operator-= (const Vector3& other);
-	inline Vector3& operator/= (const Vector3& other);
-	inline Vector3& operator*= (const Vector3& other);
+	inline Vector3& operator= (const Vector3& _other);
+	inline Vector3& operator+= (const Vector3& _other);
+	inline Vector3& operator-= (const Vector3& _other);
+	inline Vector3& operator/= (const Vector3& _other);
+	inline Vector3& operator*= (const Vector3& _other);
 
-	inline Vector3& operator/= (float scaler);
-	inline Vector3& operator*= (float scaler);
+	inline Vector3& operator/= (float _scaler);
+	inline Vector3& operator*= (float _scaler);
 
 	inline Vector3 operator- () const;
 	inline Vector3 operator+ () const;
 
 };
 
-
-
 /// ===================================================
 /// 演算子オーバーロード
 /// ===================================================
 #pragma region operator overload
 
-inline Vector3 operator+ (const Vector3& v1, const Vector3& v2) {
+inline Vector3 operator+ (const Vector3& _v1, const Vector3& _v2) {
 	return {
-		v1.x + v2.x,
-		v1.y + v2.y,
-		v1.z + v2.z
+		_v1.x + _v2.x,
+		_v1.y + _v2.y,
+		_v1.z + _v2.z
 	};
 }
 
 
-inline Vector3 operator- (const Vector3& v1, const Vector3& v2) {
+inline Vector3 operator- (const Vector3& _v1, const Vector3& _v2) {
 	return {
-		v1.x - v2.x,
-		v1.y - v2.y,
-		v1.z - v2.z
+		_v1.x - _v2.x,
+		_v1.y - _v2.y,
+		_v1.z - _v2.z
 	};
 }
 
 
-inline Vector3 operator/ (const Vector3& v1, const Vector3& v2) {
+inline Vector3 operator/ (const Vector3& _v1, const Vector3& _v2) {
 	return {
-		v1.x / v2.x,
-		v1.y / v2.y,
-		v1.z / v2.z
+		_v1.x / _v2.x,
+		_v1.y / _v2.y,
+		_v1.z / _v2.z
 	};
 }
 
 
-inline Vector3 operator/ (const Vector3& v, float scaler) {
+inline Vector3 operator/ (const Vector3& _v, float _scaler) {
 	return {
-		v.x / scaler,
-		v.y / scaler,
-		v.z / scaler
+		_v.x / _scaler,
+		_v.y / _scaler,
+		_v.z / _scaler
 	};
 }
 
 
-inline Vector3 operator* (const Vector3& v1, const Vector3& v2) {
+inline Vector3 operator* (const Vector3& _v1, const Vector3& _v2) {
 	return {
-		v1.x * v2.x,
-		v1.y * v2.y,
-		v1.z * v2.z
+		_v1.x * _v2.x,
+		_v1.y * _v2.y,
+		_v1.z * _v2.z
 	};
 }
 
 
-inline Vector3 operator* (const Vector3& v, float scaler) {
+inline Vector3 operator* (const Vector3& _v, float _scaler) {
 	return {
-		v.x * scaler,
-		v.y * scaler,
-		v.z * scaler
+		_v.x * _scaler,
+		_v.y * _scaler,
+		_v.z * _scaler
 	};
 }
 
 
-inline Vector3 operator* (float scaler, const Vector3& v) {
-	return v * scaler;
+inline Vector3 operator* (float _scaler, const Vector3& _v) {
+	return _v * _scaler;
 }
 
 
-inline bool operator!=(const Vector3& v1, const Vector3& v2) {
-	return v1.x != v2.x || v1.y != v2.y || v1.z != v2.z;
+inline bool operator!=(const Vector3& _v1, const Vector3& _v2) {
+	return _v1.x != _v2.x || _v1.y != _v2.y || _v1.z != _v2.z;
 }
 
-inline bool operator==(const Vector3& v1, const Vector3& v2) {
-	return v1.x == v2.x && v1.y == v2.y && v1.z == v2.z;
+inline bool operator==(const Vector3& _v1, const Vector3& _v2) {
+	return _v1.x == _v2.x && _v1.y == _v2.y && _v1.z == _v2.z;
 }
 
 
@@ -163,46 +161,46 @@ inline bool operator==(const Vector3& v1, const Vector3& v2) {
 /// Vector3 : operator overload
 /// ===================================================
 
-inline Vector3& Vector3::operator= (const Vector3& other) {
-	this->x = other.x;
-	this->y = other.y;
-	this->z = other.z;
+inline Vector3& Vector3::operator= (const Vector3& _other) {
+	this->x = _other.x;
+	this->y = _other.y;
+	this->z = _other.z;
 	return *this;
 }
 
 
-inline Vector3& Vector3::operator+= (const Vector3& other) {
-	(*this) = *this + other;
+inline Vector3& Vector3::operator+= (const Vector3& _other) {
+	(*this) = *this + _other;
 	return *this;
 }
 
 
-inline Vector3& Vector3::operator-= (const Vector3& other) {
-	(*this) = *this - other;
+inline Vector3& Vector3::operator-= (const Vector3& _other) {
+	(*this) = *this - _other;
 	return *this;
 }
 
 
-inline Vector3& Vector3::operator/= (const Vector3& other) {
-	(*this) = *this / other;
+inline Vector3& Vector3::operator/= (const Vector3& _other) {
+	(*this) = *this / _other;
 	return *this;
 }
 
 
-inline Vector3& Vector3::operator*= (const Vector3& other) {
-	(*this) = *this * other;
+inline Vector3& Vector3::operator*= (const Vector3& _other) {
+	(*this) = *this * _other;
 	return *this;
 }
 
 
-inline Vector3& Vector3::operator/= (float scaler) {
-	(*this) = *this / scaler;
+inline Vector3& Vector3::operator/= (float _scaler) {
+	(*this) = *this / _scaler;
 	return *this;
 }
 
 
-inline Vector3& Vector3::operator*= (float scaler) {
-	(*this) = *this * scaler;
+inline Vector3& Vector3::operator*= (float _scaler) {
+	(*this) = *this * _scaler;
 	return *this;
 }
 
