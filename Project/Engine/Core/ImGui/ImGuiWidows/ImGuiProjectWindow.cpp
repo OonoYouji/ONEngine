e904f6144549f432818400130eceda622e92c7a9
OonoYouji <k023g0007@g.neec.ac.jp>
2025-03-13 14:34:23 +0900

    適当にAssetsを読み込んで階層構造を表示してみた

--- Project/Engine/Core/ImGui/ImGuiWidows/ImGuiProjectWindow.cpp
+++ Project/Engine/Core/ImGui/ImGuiWidows/ImGuiProjectWindow.cpp
@@ -1,7 +1,16 @@
 #include "ImGuiProjectWindow.h"
 
+/// stl
+#include <filesystem>
+
 /// external
 #include <imgui.h>
+
+ImGuiProjectWindow::ImGuiProjectWindow() {
+	rootFolder_ = std::make_shared<Folder>();
+	rootFolder_->name = "root";
+	LoadFolder("./Assets", rootFolder_);
+}
 
 void ImGuiProjectWindow::ImGuiFunc() {
 	ImGui::Begin("project", nullptr, ImGuiWindowFlags_AlwaysAutoResize);
@@ -34,6 +43,10 @@ void ImGuiProjectWindow::ImGuiFunc() {
 
 void ImGuiProjectWindow::Hierarchy() {
 	ImGui::BeginChild("Hierarchy", ImVec2(0, -FLT_MIN), true, ImGuiWindowFlags_HorizontalScrollbar);
+
+	/// root folderを再帰的に描画
+	DrawFolder(rootFolder_, 0);
+
 	ImGui::EndChild();
 }
 
@@ -42,3 +55,38 @@ void ImGuiProjectWindow::SelectFileView() {
 	ImGui::EndChild();
 }
 
+void ImGuiProjectWindow::LoadProject(const std::string& _path) {
+	LoadFolder(_path, rootFolder_);
+}
+
+
+void ImGuiProjectWindow::LoadFolder(const std::string& _path, std::shared_ptr<Folder> _folder) {
+	for (const auto& entry : std::filesystem::directory_iterator(_path)) {
+		if (entry.is_directory()) {
+
+			auto subFolder = std::make_shared<Folder>();
+			subFolder->name = entry.path().filename().string();
+			_folder->folders.push_back(subFolder);
+			LoadFolder(entry.path().string(), subFolder);
+
+		} else if (entry.is_regular_file()) {
+
+			File file;
+			file.name = entry.path().filename().string();
+			_folder->files.push_back(file);
+		}
+	}
+}
+
+void ImGuiProjectWindow::DrawFolder(std::shared_ptr<Folder> _folder, size_t _depth) {
+	ImGui::SetNextItemOpen(true, ImGuiCond_Once);
+	if (ImGui::TreeNodeEx(_folder->name.c_str(), ImGuiTreeNodeFlags_DefaultOpen)) {
+		for (const auto& subFolder : _folder->folders) {
+			DrawFolder(subFolder, _depth + 1);
+		}
+		for (const auto& file : _folder->files) {
+			ImGui::BulletText("%s", file.name.c_str());
+		}
+		ImGui::TreePop();
+	}
+}
--- Project/Engine/Core/ImGui/ImGuiWidows/ImGuiProjectWindow.h
+++ Project/Engine/Core/ImGui/ImGuiWidows/ImGuiProjectWindow.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <string>
+
 /// engine
 #include "Collection/ImGuiWindowCollection.h"
 
@@ -7,12 +9,27 @@
 /// ImGuiProjectWindow
 /// ///////////////////////////////////////////////////
 class ImGuiProjectWindow : public IImGuiWindow {
+	/// ===================================================
+	/// private : sub class
+	/// ===================================================
+
+	struct File {
+		std::string name;
+	};
+
+	struct Folder {
+		std::string name;
+		std::vector<std::shared_ptr<Folder>> folders;
+		std::vector<File> files;
+	};
+
+
 public:
 	/// ===================================================
 	/// public : methods
 	/// ===================================================
 
-	ImGuiProjectWindow() {}
+	ImGuiProjectWindow();
 	~ImGuiProjectWindow() {}
 
 	/// @brief imgui windowの描画処理
@@ -29,5 +46,29 @@ private:
 	/// @brief プロジェクトのファイル表示
 	void SelectFileView();
 
+
+
+	/// @brief プロジェクトの読み込み
+	/// @param _path 読み込むプロジェクトのパス
+	void LoadProject(const std::string& _path);
+
+	/// @brief フォルダの読み込み
+	/// @param _path 読み込むフォルダのパス
+	void LoadFolder(const std::string& _path, std::shared_ptr<Folder> _folder);
+
+
+	/// @brief フォルダーの描画
+	/// @param _folder 描画するフォルダ
+	void DrawFolder(std::shared_ptr<Folder> _folder, size_t _depth);
+
+private:
+	/// ===================================================
+	/// private : objects
+	/// ===================================================
+	
+	std::shared_ptr<Folder> rootFolder_;
+
 };
+
+
 
