f222c0996212031235fd7ec25e19a782250d8896
OonoYouji <k023g0007@g.neec.ac.jp>
2025-03-22 03:59:44 +0900

    GameSceneのデバッグウィンドウを元の配置に戻した

--- Project/Engine/Core/ImGui/ImGuiWidows/GameWindows/ImGuiGameWindow.cpp
+++ Project/Engine/Core/ImGui/ImGuiWidows/GameWindows/ImGuiGameWindow.cpp
@@ -12,6 +12,13 @@
 
 ImGuiGameWindow::ImGuiGameWindow(EntityCollection* _entityCollection, GraphicsResourceCollection* _resourceCollection) {
 
+	/// windowの設定
+	imGuiFlags_ |= ImGuiWindowFlags_NoMove;
+	imGuiFlags_ |= ImGuiWindowFlags_NoResize;
+	imGuiFlags_ |= ImGuiWindowFlags_NoTitleBar;
+	imGuiFlags_ |= ImGuiWindowFlags_NoBringToFrontOnFocus;
+
+	/// ここでwindowを生成する
 	AddChild(std::make_unique<ImGuiProjectWindow>());
 	AddChild(std::make_unique<ImGuiGameSceneWindow>());
 	AddChild(std::make_unique<ImGuiSceneWindow>(_resourceCollection));
@@ -22,13 +29,22 @@ ImGuiGameWindow::ImGuiGameWindow(EntityCollection* _entityCollection, GraphicsRe
 
 
 void ImGuiGameWindow::ImGuiFunc() {
-	if (!ImGui::Begin("Game")) {
+
+	ImGui::SetNextWindowPos(ImVec2(0, 20), ImGuiCond_FirstUseEver);
+	ImGui::SetNextWindowSize(ImVec2(1280, 700), ImGuiCond_FirstUseEver);
+	if (!ImGui::Begin("Game", nullptr, imGuiFlags_)) {
 		ImGui::End();
 		return;
 	}
 
 	ImGuiID dockspaceID = ImGui::GetID("GameDockingSpace");
 	ImGui::DockSpace(dockspaceID, ImVec2(0.0f, 0.0f));
+
+	///// ウィンドウの描画
+	//ImGui::SetWindowPos("Game", ImVec2(0, 20), ImGuiCond_Always);
+	//ImGui::SetWindowSize("Game", ImVec2(1280, 700), ImGuiCond_Always);
+	//ImGui::SetWindowFocus(nullptr);
+
 
 	UpdateChildren();
 
