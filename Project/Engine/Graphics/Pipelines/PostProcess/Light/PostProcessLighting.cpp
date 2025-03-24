52e79c0dbc774554a8e9a6eb0b54eca38636dfb4
OonoYouji <k023g0007@g.neec.ac.jp>
2025-03-25 01:43:07 +0900

    shader部分は記述したのでそれに合わせてCPU側のセットを行う

--- Project/Engine/Graphics/Pipelines/PostProcess/Light/PostProcessLighting.cpp
+++ Project/Engine/Graphics/Pipelines/PostProcess/Light/PostProcessLighting.cpp
@@ -14,9 +14,24 @@ void PostProcessLighting::Initialize(ShaderCompiler* _shaderCompiler, DxManager*
 
 		Shader shader;
 		shader.Initialize(_shaderCompiler);
-		shader.CompileShader(L"Assets/Shader/PostProcess/Lighting/Lighting.cs.hlsl", L"cs_6_0", Shader::Type::cs);
+		shader.CompileShader(L"Assets/Shader/PostProcess/Lighting/Lighting.cs.hlsl", L"cs_6_6", Shader::Type::cs);
 
 		pipeline_->SetShader(&shader);
+
+		pipeline_->AddCBV(D3D12_SHADER_VISIBILITY_ALL, 0);
+		pipeline_->AddCBV(D3D12_SHADER_VISIBILITY_ALL, 1);
+
+		pipeline_->AddDescriptorRange(0, 1, D3D12_DESCRIPTOR_RANGE_TYPE_SRV);
+		pipeline_->AddDescriptorRange(1, 1, D3D12_DESCRIPTOR_RANGE_TYPE_SRV);
+		pipeline_->AddDescriptorRange(2, 1, D3D12_DESCRIPTOR_RANGE_TYPE_SRV);
+		pipeline_->AddDescriptorRange(0, 1, D3D12_DESCRIPTOR_RANGE_TYPE_UAV);
+
+		pipeline_->AddDescriptorTable(D3D12_SHADER_VISIBILITY_ALL, 0);
+		pipeline_->AddDescriptorTable(D3D12_SHADER_VISIBILITY_ALL, 1);
+		pipeline_->AddDescriptorTable(D3D12_SHADER_VISIBILITY_ALL, 2);
+		pipeline_->AddDescriptorTable(D3D12_SHADER_VISIBILITY_ALL, 3);
+
+		pipeline_->AddStaticSampler(D3D12_SHADER_VISIBILITY_ALL, 0);
 	
 		pipeline_->CreatePipeline(_dxManager->GetDxDevice());
 	}
@@ -25,5 +40,5 @@ void PostProcessLighting::Initialize(ShaderCompiler* _shaderCompiler, DxManager*
 
 void PostProcessLighting::Execute(DxCommand* _dxCommand) {
 	pipeline_->SetPipelineStateForCommandList(_dxCommand);
-	_dxCommand->GetCommandList()->Dispatch(1280 / 32, 720 / 32, 1);
+ 	_dxCommand->GetCommandList()->Dispatch(1280 / 32, 720 / 32, 1);
 }
