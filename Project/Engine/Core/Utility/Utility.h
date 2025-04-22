774c96d57ddd03ea504bda490a52bcef7b6dbc45
OonoYouji <k023g0007@g.neec.ac.jp>
2025-04-22 11:48:23 +0900

    utility用のinclude headerを作った player walk effectの動きを作った

---
+++ Project/Engine/Core/Utility/Utility.h
@@ -0,0 +1,19 @@
+#pragma once
+
+/// input
+#include "Input/Input.h"
+
+/// math
+#include "Math/Vector2.h"
+#include "Math/Vector3.h"
+#include "Math/Vector4.h"
+#include "Math/Matrix4x4.h"
+#include "Math/Quaternion.h"
+
+/// time
+#include "Time/Time.h"
+
+/// tools
+#include "Tools/Assert.h"
+#include "Tools/Log.h"
+//#include "Tools/Random.h"
--- Project/Game/Effects/PlaneEffects/PlayerWalkEffect.cpp
+++ Project/Game/Effects/PlaneEffects/PlayerWalkEffect.cpp
@@ -2,20 +2,22 @@
 
 #define NOMINMAX
 #include "Engine/ECS/Component/Component.h"
+#include "Engine/Core/Utility/Utility.h"
 
 void PlayerWalkEffect::Initialize() {
 	Effect* effect = AddComponent<Effect>();
-	effect->SetEmitTypeDistance(10.0f, 4);
+	effect->SetEmitTypeDistance(2, 4);
 	effect->SetMeshPath("Assets/Models/primitive/plane.obj");
 	effect->SetTexturePath("Packages/Textures/uvChecker.png");
 
 	effect->SetStartSpeed(0.1f);
-	effect->SetLifeLeftTime(0.2f);
+	effect->SetLifeLeftTime(0.5f);
 
 	effect->SetElementUpdateFunc(
 		[](Effect::Element* _element) {
 			_element->color.w = std::max(0.0f, _element->lifeTime / 3.0f);
 
+			_element->transform.position.y += 0.1f * Time::DeltaTime();
 			_element->transform.scale.z = 3.0f * (1.0f + (1.0f - _element->lifeTime / 0.2f));
 
 			if (_element->transform.rotate.y == 0) {
