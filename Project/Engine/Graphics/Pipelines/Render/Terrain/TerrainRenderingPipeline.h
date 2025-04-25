edf4830f84a484c4e10cb0a20e655e46bc584d18
OonoYouji <k023g0007@g.neec.ac.jp>
2025-04-25 14:18:07 +0900

    Terrainの描画pipelineクラスを追加

---
+++ Project/Engine/Graphics/Pipelines/Render/Terrain/TerrainRenderingPipeline.h
@@ -0,0 +1,28 @@
+#pragma once
+
+/// engine
+#include "../../Interface/IRenderingPipeline.h"
+
+
+/// ////////////////////////////////////
+/// Terrainの描画pipeline
+/// ////////////////////////////////////
+class TerrainRenderingPipeline : public IRenderingPipeline {
+public:
+	/// ====================================
+	/// public : methods
+	/// ====================================
+
+	TerrainRenderingPipeline();
+	~TerrainRenderingPipeline();
+
+
+private:
+	/// ====================================
+	/// private : objects
+	/// ====================================
+
+
+
+};
+
