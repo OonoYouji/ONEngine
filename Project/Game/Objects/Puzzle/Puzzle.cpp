2feb371e50fae85c311f307bc88880ab11dce9e0
OonoYouji <k023g0007@g.neec.ac.jp>
2025-06-06 15:27:02 +0900

    puzzleのオブジェクトクラスを追加

---
+++ Project/Game/Objects/Puzzle/Puzzle.cpp
@@ -0,0 +1,14 @@
+#include "Puzzle.h"
+
+/// engine
+#include "Engine/Core/Utility/Utility.h"
+#include "Engine/ECS/Component/Component.h"
+
+
+void Puzzle::Initialize() {
+
+}
+
+void Puzzle::Update() {
+
+}
---
+++ Project/Game/Objects/Puzzle/Puzzle.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "Engine/ECS/EntityComponentSystem/EntityComponentSystem.h"
+
+/// ////////////////////////////////////////////////
+/// Puzzle
+/// ////////////////////////////////////////////////
+class Puzzle : public IEntity {
+public:
+	/// =========================================
+	/// public : methods
+	/// =========================================
+	
+	Puzzle() = default;
+	~Puzzle() override = default;
+
+	void Initialize() override;
+	void Update() override;
+
+private:
+	/// =========================================
+	/// private : objects
+	/// =========================================
+
+};
+
