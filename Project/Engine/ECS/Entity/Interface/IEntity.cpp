c1be0c75b345861b3d2c6a29b67e202bc1c7b4dd
OonoYouji <k023g0007@g.neec.ac.jp>
2025-06-13 14:19:27 +0900

    entityにidを持たせるようにした

--- Project/Engine/ECS/Entity/Interface/IEntity.cpp
+++ Project/Engine/ECS/Entity/Interface/IEntity.cpp
@@ -13,8 +13,6 @@ void IEntity::CommonInitialize() {
 	transform_ = AddComponent<Transform>();
 	variables_ = AddComponent<Variables>();
 	variables_->LoadJson("./Assets/Jsons/" + name_ + ".json");
-
-	id_ = static_cast<uint32_t>(reinterpret_cast<uintptr_t>(this));
 }
 
 IComponent* IEntity::AddComponent(const std::string& _name) {
@@ -222,6 +220,6 @@ bool IEntity::GetActive() const {
 	return active_;
 }
 
-uint32_t IEntity::GetId() const {
+size_t IEntity::GetId() const {
 	return id_;
 }
