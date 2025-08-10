225d432ccb81dda23dd1de4fba39c1a818d383db
OonoYouji <k023g0007@g.neec.ac.jp>
2025-08-11 04:19:01 +0900

    地形の勾配を得る関数を追加 コライダーに前フレームの座標を保持する変数を追加

--- Project/Engine/ECS/Component/Components/ComputeComponents/Collision/ICollider.cpp
+++ Project/Engine/ECS/Component/Components/ComputeComponents/Collision/ICollider.cpp
@@ -1,5 +1,19 @@
 #include "ICollider.h"
 
+/// engine
+#include "Engine/ECS/Entity/Interface/IEntity.h"
+
+void ICollider::UpdatePrevPosition() {
+	/// 前フレームの座標を更新する
+	if (IEntity* owner = GetOwner()) {
+		prevPosition_ = owner->GetTransform()->GetPosition();
+	}
+}
+
+const Vector3& ICollider::GetPrevPosition() const {
+	return prevPosition_;
+}
+
 void ICollider::SetOnCollisionEnterCallBackFunc(ColliderCallBackFunc _func) {
 	onCollisionEnterCallBackFunc_ = _func;
 }
