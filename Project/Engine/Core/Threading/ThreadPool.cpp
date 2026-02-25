634db7cf18b17ed93459a351f1e7bf17c58252c2
OonoYouji <k023g0007@g.neec.ac.jp>
2026-02-26 05:39:27 +0900

    Threadの定義作成

---
+++ Project/Engine/Core/Threading/ThreadPool.cpp
@@ -0,0 +1,53 @@
+﻿#include "ThreadPool.h"
+
+namespace ONEngine {
+
+ThreadPool& ThreadPool::Instance() {
+    static ThreadPool instance;
+    return instance;
+}
+
+ThreadPool::~ThreadPool() {
+    Shutdown();
+}
+
+void ThreadPool::Initialize(size_t threadCount) {
+    if(running_) return;
+
+    running_ = true;
+    threadCount_ = threadCount;
+
+    for(size_t i = 0; i < threadCount_; ++i) {
+        workers_.emplace_back(&ThreadPool::WorkerLoop, this);
+    }
+}
+
+void ThreadPool::Shutdown() {
+    if(!running_) return;
+
+    running_ = false;
+
+    // 空ジョブを入れてスレッドを起こす
+    for(size_t i = 0; i < workers_.size(); ++i) {
+        jobQueue_.Push([] {});
+    }
+
+    for(auto& t : workers_) {
+        if(t.joinable()) t.join();
+    }
+
+    workers_.clear();
+}
+
+void ThreadPool::WorkerLoop() {
+    while(running_) {
+        std::function<void()> job;
+        if(jobQueue_.TryPop(job)) {
+            job();
+        } else {
+            std::this_thread::yield();
+        }
+    }
+}
+
+} /// namespeace ONEngine
---
+++ Project/Engine/Core/Threading/ThreadPool.h
@@ -0,0 +1,60 @@
+﻿#pragma once
+
+/// std
+#include <vector>
+#include <thread>
+#include <future>
+#include <functional>
+#include <atomic>
+#include <memory>
+
+/// engine
+#include "ConcurrentQueue.h"
+
+
+namespace ONEngine {
+
+class ThreadPool {
+public:
+	static ThreadPool& Instance();
+
+	void Initialize(size_t threadCount = std::thread::hardware_concurrency());
+	void Shutdown();
+
+	// 任意の関数を非同期実行
+	template<class F, class... Args>
+	auto Enqueue(F&& f, Args&&... args) -> std::future<std::invoke_result_t<F, Args...>> {
+		using ReturnType = std::invoke_result_t<F, Args...>;
+
+		auto task = std::make_shared<std::packaged_task<ReturnType()>>(
+			std::bind(std::forward<F>(f), std::forward<Args>(args)...)
+		);
+
+		std::future<ReturnType> result = task->get_future();
+
+		jobQueue_.Push([task]() { (*task)(); });
+
+		return result;
+	}
+
+	size_t GetThreadCount() const { return threadCount_; }
+
+private:
+	ThreadPool() = default;
+	~ThreadPool();
+
+	ThreadPool(const ThreadPool&) = delete;
+	ThreadPool& operator=(const ThreadPool&) = delete;
+
+	void WorkerLoop();
+
+private:
+	std::vector<std::thread> workers_;
+	ConcurrentQueue<std::function<void()>> jobQueue_;
+
+	std::atomic<bool> running_ = false;
+	size_t threadCount_ = 0;
+};
+
+
+} /// namespeace ONEngine
